381c6da5334f16c76aa57077553485ed61ff99f1
jlewis198705-netizen <jlewis198705-netizen@users.noreply.github.com>
2026-06-19 16:11:18 -0600

    Corregir nombre de variable de base de datos

--- conectarte_project/settings.py
+++ conectarte_project/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,12 +62,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'conectarte_project.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-    )
-}
+db_from_env = dj_database_url.config(conn_max_age=600)
+
+if db_from_env:
+    DATABASES = {
+        'default': db_from_env
+    }
+else:
+    # Si no hay URL (como en tu PC local), usamos SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
